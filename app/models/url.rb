01b021fbaf517fba25b05ebf6838915e6888adc0
Charles Darwin D.C. Pobre <cpobre.peg@gmail.com>
2018-05-24 23:07:52 +0800

    Generate Short URL upon saving

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,13 @@
 class Url < ApplicationRecord
+  validates_format_of :original_url,
+    with: /\A(?:(?:http|https):\/\/)?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[-a-zA-Z0-9@,!:%_\+.~#?&\/\/=]*)?\z/
+
+  before_create :generate_short_url
+
+  def generate_short_url
+		self.shorten_url = loop do
+			random_str = SecureRandom.base58(7)
+			break random_str unless self.class.exists?(shorten_url: random_str)
+		end
+  end
 end
